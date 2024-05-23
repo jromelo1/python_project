6ac3e72708073f6ec6d7f523aaee738056d962b2
jromelo1 <jessica.romerol85@gmail.com>
2024-05-23 03:23:18 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+# Leer los datos
+car_data = pd.read_csv('vehicles_us.csv')
+
+# Crear un botón
+hist_button = st.button('Construir histograma')
+
+if hist_button:  # Al hacer clic en el botón
+    # Escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    # Crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+    
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# Crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+
+
+
+
