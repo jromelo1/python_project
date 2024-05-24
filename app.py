a3d09b128b42d2566af25889e31e0cfd40e6d8cf
jromelo1 <jessica.romerol85@gmail.com>
2024-05-24 23:46:25 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if hist_button:  # Al hacer clic en el botón
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     
     # Crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer", y="price")
     
     # Mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
